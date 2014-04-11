633db6ed4368cde1b5bc12d0a6c67ca82aa85771
Kazuya Arakawa <arakawa.vyw@gmail.com>
2014-04-12 00:02:12 +0900

    controller/view request_methods

--- app/controllers/request_methods_controller.rb
+++ app/controllers/request_methods_controller.rb
@@ -1,16 +1,23 @@
 class RequestMethodsController < ApplicationController
+  add_breadcrumb "home", :root_path
+  add_breadcrumb "リクエストメソッド", :request_methods_path
+
   def index
   end
 
   def get
+    render action: :index
   end
 
   def post
+    render action: :index
   end
 
   def put
+    render action: :index
   end
 
   def delete
+    render action: :index
   end
 end
