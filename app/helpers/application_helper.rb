dadcc4028724e204a2b4ca220e1f5f07806c7b75
Kazuya Arakawa <arakawa.vyw@gmail.com>
2014-04-11 22:17:20 +0900

    show http request headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def request_headers
+    http_headers = request.env.select { |k, v| k.start_with?('HTTP_') }
+    headers = http_headers.inject({}) do |a, (k, v)|
+      a[k.sub(/^HTTP_/, "").downcase.gsub(/(^|_)\w/) { |word| word.upcase }.gsub("_", "-") ] = v
+      a
+    end
+    # puts headers.to_a.map { |v| v.join(": ") }.join("\n")
+  end
 end
